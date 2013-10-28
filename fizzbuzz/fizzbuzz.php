1e723ea83659e0c82b82efda04ccad728d0fee8f
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-28 19:06:38 +0900

    3までテストとおる

---
+++ fizzbuzz/fizzbuzz.php
@@ -0,0 +1,14 @@
+<?php
+
+class FizzBuzz
+{
+    function detect($number)
+    {
+        if ($number === 3)
+        {
+            return 'Fizz';
+        }
+        return $number;
+    }
+}
+
--- fizzbuzz/fizzbuzz_test.php
+++ fizzbuzz/fizzbuzz_test.php
@@ -4,9 +4,14 @@ require_once("../simpletest/autorun.php");
 require_once("../simpletest/unit_tester.php");
 require_once("../simpletest/reporter.php");
 
+require_once("fizzbuzz.php");
+
 class TestFizzBuzz extends UnitTestCase {
-    function test1()
+    function testDetect()
     {
-        $this->assertEqual(1, 1);
+        $fizzbuzz = new FizzBuzz();
+        $this->assertEqual($fizzbuzz->detect(1), 1);
+        $this->assertEqual($fizzbuzz->detect(2), 2);
+        $this->assertEqual($fizzbuzz->detect(3), "Fizz");
     }
 }
